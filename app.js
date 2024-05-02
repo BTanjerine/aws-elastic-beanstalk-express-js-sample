a2027bd0061a91545dd336cb7a6f6163b1e9e448
Kenji <kickthepokehi@hotmail.com>
2024-05-01 19:28:24 -0700

    made a html page

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Get this message and take it!!'));
+app.get('/', function (req, res) { 
+    res.send('Get this message and take it!!')
+    res.sendFile(path.join(__dirname, './index.html'))
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
